23f4d4dea42a1dd6ac9c402ae75d071caca03dac
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-11-05 10:35:39 -0300

    Adds show action to the users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  def default_url_options
+    if Rails.env.production?
+      { host: 'example.com' }
+    else
+      { host: 'localhost:3000' }
+    end
+  end
 end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -4,16 +4,17 @@ require 'rails_helper'
 require 'json'
 
 RSpec.describe 'Api::V1::Users', type: :request do
+  fixtures :users
   let(:user) { users(:one) }
 
   describe 'GET /index' do
     context 'when is succesful' do
       it 'will show user' do
-        get api_v1_user_url(:user).to_json
+        get api_v1_user_path(user)
         assert_response :success
         # Test to ensure response contains the correct email
         json_response = JSON.parse(response.body)
-        assert_equal :user.email, json_response['email']
+        assert_equal user.email, json_response['email']
       end
     end
   end
