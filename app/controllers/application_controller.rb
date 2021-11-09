9fd65426dc3c3347d8c333457a05d5bc080d6ca1
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-11-09 09:04:13 -0300

    Delete unnecessary config in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  def default_url_options
-    if Rails.env.production?
-      { host: 'example.com' }
-    else
-      { host: 'localhost:3000' }
-    end
-  end
 end
