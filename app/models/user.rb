ba6f59c4b6cf9ec36c77d31e5eac2db570877a9c
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-11-09 09:42:37 -0300

    Add email validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-  validates :email, uniqueness: true
-  validates_format_of :email, with: /@/
+  validates :email, uniqueness: true, email: true
   validates :password_digest, presence: true
 
   has_secure_password
