98167e0bdb040e42525190b62bf103c473a4e90e
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-11-10 17:37:19 -0300

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-if User.count.zero?
-  100.times do
-    User.create!(
-      email: Faker::Internet.email, password: Faker::Internet.password
-    )
-  end
+# if User.count.zero?
+5.times do
+  User.create!(
+    email: Faker::Internet.email, password: Faker::Internet.password
+  )
 end
+# end
