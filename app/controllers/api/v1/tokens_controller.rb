c54050338e7dd45e4b1795037ef1386f7ec7563b
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-11-10 17:09:23 -0300

    Add create token, route and fix spec

---
+++ app/controllers/api/v1/tokens_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class Api::V1::TokensController < ApplicationController
+  def create
+    user = User.find_by_email(user_params[:email])
+    if user&.authenticate(user_params[:password])
+      render json: {
+        token: JsonWebToken.encode(user_id: user.id),
+        email: user.email
+      }
+    else
+      head :unauthorized
+    end
+  end
+
+  private
+
+  # Only allow a trusted parameter "white list" through.
+  def user_params
+    params.require(:user).permit(:email, :password, :id)
+  end
+
+  def user
+    @user ||= User.find(params[:id])
+  end
+end
