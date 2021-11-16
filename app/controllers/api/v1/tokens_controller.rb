6f2a337edfe1449597825ca678596886173b2198
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-11-16 12:59:58 -0300

    Fixes in tokens controller

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -2,15 +2,13 @@
 
 class Api::V1::TokensController < ApplicationController
   def create
-    user = User.find_by_email(user_params[:email])
-    if user&.authenticate(user_params[:password])
-      render json: {
-        token: JsonWebToken.encode(user_id: user.id),
-        email: user.email
-      }
-    else
-      head :unauthorized
-    end
+    authenticated = user&.authenticate(user_params[:password])
+    return head :unauthorized unless authenticated
+
+    render json: {
+      token: JsonWebToken.encode(user_id: user.id),
+      email: user.email
+    }
   end
 
   private
@@ -21,6 +19,6 @@ class Api::V1::TokensController < ApplicationController
   end
 
   def user
-    @user ||= User.find(params[:id])
+    @user ||= User.find_by_email(user_params[:email])
   end
 end
