d291c0fb0f66736c39ca45aaf5e42b470e7b5131
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-11-18 15:18:06 -0300

    Add new method to auth user with prods + fix specs

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::ProductsController, type: :controller do
   def user
-    @user ||= create(:user, :with_products)
+    @user ||= create_authenticated_user_with_products
   end
 
   def product
@@ -12,30 +12,35 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
   end
 
   describe 'GET #show' do
-    context 'when is successful' do
-      before { get :show, params: { id: product.id } }
+    def do_request(id)
+      get :show, params: { id: id }
+    end
 
+    context 'when is successful' do
       it 'return successful' do
+        do_request(product.id)
         expect(response).to have_http_status(:success)
       end
 
       it 'returns the product' do
+        do_request(product.id)
         expect(JSON.parse(response.body)['title']).to eql(product.title)
       end
     end
 
     context 'when is not successful' do
-      before { get :show, params: { id: 0 } }
-
       it 'returns not found' do
+        do_request(0)
         expect(response).to have_http_status(:not_found)
       end
 
       it 'returns the error message' do
+        do_request(0)
         expect(JSON.parse(response.body)['details']).to eql("Couldn't find Product with 'id'=0")
       end
 
       it 'returns the error code 4104' do
+        do_request(0)
         expect(JSON.parse(response.body)['error_code']).to be(4104)
       end
     end
@@ -43,60 +48,50 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
 
   describe 'GET #index' do
     context 'when is successful' do
-      let(:products) { user.products }
+      let!(:products) { user.products }
 
-      before do
-        # If I remove this line, the test will fail
-        user.products
-
+      def do_request
         get :index
       end
 
       it 'return successful' do
+        do_request
         expect(response).to have_http_status(:success)
       end
 
       it 'returns the list of products' do
+        do_request
         expect(JSON.parse(response.body).first['title']).to eql(products.first.title)
       end
     end
   end
 
   describe 'POST #create' do
+    def do_request(product)
+      authenticate_user(user)
+      post :create, params: product
+    end
+
     context 'when is successful' do
-      let(:headers) do
-        { 'Authorization': JsonWebToken.encode(user_id: user.id) }
-      end
-
-      before do
-        request.headers.merge! headers
-        post :create, params: { product: { title: 'New Product', price: 10 } }
-      end
-
       it 'returns the product' do
+        do_request({ product: { title: 'New Product', price: 10 } })
         expect(JSON.parse(response.body)['title']).to eql('New Product')
       end
     end
 
     context 'when is not successful' do
-      let(:headers) do
-        { 'Authorization': JsonWebToken.encode(user_id: user.id) }
-      end
-
-      before do
-        request.headers.merge! headers
-        post :create, params: { product: { title: '', price: 10 } }
-      end
-
       it 'returns the error message' do
+        do_request({ product: { title: '', price: 10 } })
         expect(JSON.parse(response.body)['description']).to eql('Invalid parameter in product')
       end
 
-      it 'returns unprocessable entity' do
+      it 'returns bad request' do
+        do_request({ product: { title: '', price: 10 } })
         expect(response).to have_http_status(:bad_request)
       end
 
       it 'returns the error code 4130' do
+        do_request({ product: { title: '', price: 10 } })
         expect(JSON.parse(response.body)['error_code']).to be(4130)
       end
     end
@@ -104,80 +99,122 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
 
   describe 'PUT #update' do
     context 'when is successful' do
-      let(:headers) do
-        { 'Authorization': JsonWebToken.encode(user_id: user.id) }
-      end
-
-      before do
-        request.headers.merge! headers
-        put :update, params: { id: product.id, product: { title: 'New Product' } }
+      def do_request(id, product)
+        authenticate_user(user)
+        put :update, params: { id: id, product: product }
       end
 
       it 'returns the product' do
+        do_request(product.id, { title: 'New Product' })
         expect(JSON.parse(response.body)['title']).to eql('New Product')
       end
     end
 
-    context 'when is not successful' do
-      let(:headers) do
-        { 'Authorization': JsonWebToken.encode(user_id: user.id) }
-      end
-
-      before do
-        request.headers.merge! headers
+    context 'when parameters are invalid' do
+      def do_request
+        authenticate_user(user)
         put :update, params: { id: product.id, product: { title: '' } }
       end
 
       it 'returns the error message' do
+        do_request
         expect(JSON.parse(response.body)['description']).to eql('Invalid parameter in product')
       end
 
       it 'returns bad request' do
+        do_request
         expect(response).to have_http_status(:bad_request)
       end
 
       it 'returns the error code 4130' do
+        do_request
         expect(JSON.parse(response.body)['error_code']).to be(4130)
+      end
+    end
+
+    context 'when user is not the owner' do
+      def do_request
+        other_user = create(:user, :with_products)
+        other_product = other_user.products.first
+        authenticate_user(user)
+        put :update, params: { id: other_product.id, product: { title: 'New Product' } }
+      end
+
+      it 'returns the error message' do
+        do_request
+        message_expected = 'Product can not be deleted or updated due to being unauthorized'
+        expect(JSON.parse(response.body)['description']).to eql(message_expected)
+      end
+
+      it 'returns unauthorized' do
+        do_request
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      it 'returns the error code 4111' do
+        do_request
+        expect(JSON.parse(response.body)['error_code']).to be(4111)
       end
     end
   end
 
   describe 'DELETE #destroy' do
     context 'when is successful' do
-      let(:headers) do
-        { 'Authorization': JsonWebToken.encode(user_id: user.id) }
-      end
-
-      before do
-        request.headers.merge! headers
+      def do_request
+        authenticate_user(user)
         delete :destroy, params: { id: product.id }
       end
 
       it 'returns no content' do
+        do_request
         expect(response).to have_http_status(:no_content)
       end
     end
 
-    context 'when is not successful' do
-      let(:headers) do
-        { 'Authorization': JsonWebToken.encode(user_id: user.id) }
-      end
-
-      before do
-        request.headers.merge! headers
+    context 'when product does not exist' do
+      def do_request
+        authenticate_user(user)
         delete :destroy, params: { id: 0 }
       end
 
       it 'returns not found' do
+        do_request
         expect(response).to have_http_status(:not_found)
       end
 
       it 'returns the error message' do
+        do_request
         expect(JSON.parse(response.body)['details']).to eql("Couldn't find Product with 'id'=0")
       end
 
       it 'returns the error code 4104' do
+        do_request
         expect(JSON.parse(response.body)['error_code']).to be(4104)
+      end
+    end
+
+    context 'when user is not the owner' do
+      def do_request
+        other_user = create(:user, :with_products)
+        other_product = other_user.products.first
+        authenticate_user(user)
+        delete :destroy, params: { id: other_product.id }
+      end
+
+      it 'returns the error message' do
+        do_request
+        message_expected = 'Product can not be deleted or updated due to being unauthorized'
+        expect(JSON.parse(response.body)['description']).to eql(message_expected)
+      end
+
+      it 'returns unauthorized' do
+        do_request
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      it 'returns the error code 4111' do
+        do_request
+        expect(JSON.parse(response.body)['error_code']).to be(4111)
       end
     end
   end
--- spec/support/support_helper.rb
+++ spec/support/support_helper.rb
@@ -8,4 +8,18 @@ module AuthenticationHelper
     @request.headers['Authorization'] = "Bearer #{jwt}"
     user
   end
+
+  def create_authenticated_user_with_products
+    user = FactoryBot.create(:user, :with_products)
+    payload = { user_id: user.id }
+    jwt = JsonWebToken.encode(payload)
+    @request.headers['Authorization'] = "Bearer #{jwt}"
+    user
+  end
+
+  def authenticate_user(user)
+    payload = { user_id: user.id }
+    jwt = JsonWebToken.encode(payload)
+    @request.headers['Authorization'] = "Bearer #{jwt}"
+  end
 end
