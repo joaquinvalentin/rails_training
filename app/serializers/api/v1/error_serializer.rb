3f4f810a38f49424dec3eabf60f25ffb579b0db5
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-11-18 14:54:52 -0300

    Stop sending http_status in body for errors

--- app/serializers/api/v1/error_serializer.rb
+++ app/serializers/api/v1/error_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Api::V1::ErrorSerializer < Blueprinter::Base
-  fields :description, :error_code, :http_status
+  fields :description, :error_code
 
   field :details, if: ->(_field_name, error, _options) { error[:details].present? }
 end
