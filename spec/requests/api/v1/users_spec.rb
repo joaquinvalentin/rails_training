0208f96015a8bb0f0498e3aa0d131002db4bbc4a
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-11-18 12:27:06 -0300

    Update doc working, delete in progress

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -3,23 +3,11 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/v1/users', type: :request do
-  describe 'Users' do
-    # path '/api/v1/users' do
-    #   # TODO: def authenticate user
-    #   post('create user') do
-    #     response(200, 'successful') do
-    #       after do |example|
-    #         example.metadata[:response][:content] = {
-    #           'application/json' => {
-    #             example: JSON.parse(response.body, symbolize_names: true)
-    #           }
-    #         }
-    #       end
+  def authentication_token(user)
+    "Bearer #{JsonWebToken.encode({ user_id: user.id })}"
+  end
 
-    #       run_test!
-    #     end
-    #   end
-    # end
+  describe 'Users' do
     path '/api/v1/users/{id}' do
       get 'Retrieve a user' do
         tags 'Users'
@@ -62,73 +50,53 @@ RSpec.describe 'api/v1/users', type: :request do
       end
     end
 
-    # path '/api/v1/users/{id}' do
-    #   # You'll want to customize the parameter types...
-    #   parameter name: 'id', in: :path, type: :string, description: 'id'
+    path '/api/v1/users/{id}' do
+      put 'Update a user' do
+        tags 'Users'
+        description 'Update a user.'
+        consumes 'application/json'
+        parameter name: :Authorization, in: :header, type: :string
+        parameter name: :id, in: :path, type: :string
+        parameter name: :params, in: :body, schema: {
+          type: :object,
+          properties: {
+            user: {
+              type: :object,
+              properties: {
+                email: { type: :string },
+                password: { type: :string }
+              },
+              required: %w[email password]
+            },
+            required: %w[user]
+          }
+        }
+        response '200', 'User updated' do
+          let(:user) { create(:user) }
+          let(:id) { user.id }
+          let(:params) { { user: attributes_for(:user) } }
+          let(:Authorization) { authentication_token(user) }
 
-    #   get('show user') do
-    #     response(200, 'successful') do
-    #       let(:id) { '123' }
+          include_context 'with integration test'
+        end
+      end
+    end
 
-    #       after do |example|
-    #         example.metadata[:response][:content] = {
-    #           'application/json' => {
-    #             example: JSON.parse(response.body, symbolize_names: true)
-    #           }
-    #         }
-    #       end
+    path '/api/v1/users/{id}' do
+      delete 'Delete a user' do
+        tags 'Users'
+        description 'Delete a user.'
+        consumes 'application/json'
+        parameter name: :Authorization, in: :header, type: :string
+        parameter name: :id, in: :path, type: :string
+        response '200', 'User deleted' do
+          let(:user) { create(:user) }
+          let(:id) { user.id }
+          let(:Authorization) { authentication_token(user) }
 
-    #       run_test!
-    #     end
-    #   end
-
-    #   patch('update user') do
-    #     response(200, 'successful') do
-    #       let(:id) { '123' }
-
-    #       after do |example|
-    #         example.metadata[:response][:content] = {
-    #           'application/json' => {
-    #             example: JSON.parse(response.body, symbolize_names: true)
-    #           }
-    #         }
-    #       end
-
-    #       run_test!
-    #     end
-    #   end
-
-    #   put('update user') do
-    #     response(200, 'successful') do
-    #       let(:id) { '123' }
-
-    #       after do |example|
-    #         example.metadata[:response][:content] = {
-    #           'application/json' => {
-    #             example: JSON.parse(response.body, symbolize_names: true)
-    #           }
-    #         }
-    #       end
-
-    #       run_test!
-    #     end
-    #   end
-
-    #   delete('delete user') do
-    #     response(200, 'successful') do
-    #       let(:id) { '123' }
-
-    #       after do |example|
-    #         example.metadata[:response][:content] = {
-    #           'application/json' => {
-    #             example: JSON.parse(response.body, symbolize_names: true)
-    #           }
-    #         }
-    #       end
-
-    #       run_test!
-    #     end
-    #   end
-    # end
+          include_context 'with integration test'
+        end
+      end
+    end
   end
 end
