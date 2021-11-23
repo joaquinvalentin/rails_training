cb9ff6992616dbf3ae7612ec8668e6b281fe5364
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-11-22 22:59:54 -0300

    Add annotate comments (users only)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ActiveRecord::Base
   validates :email, uniqueness: true, email: true
   validates :password_digest, presence: true
