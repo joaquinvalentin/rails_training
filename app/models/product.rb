8c67ee6df3d0aa9dde6c962a9d3b1d88b506db68
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-11-23 12:49:20 -0300

    Annotate Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: products
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  price      :decimal(, )      not null
+#  published  :boolean          default(FALSE)
+#  user_id    :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Product < ActiveRecord::Base
   validates :title, :user_id, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
