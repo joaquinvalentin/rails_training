303db6500bcede2062d0a900dbfbe2ea4aeb6964
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-11-23 12:21:59 -0300

    Add only param & refactor product specs

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -8,35 +8,35 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
   end
 
   describe 'GET #show' do
-    def do_request(id)
+    def make_request(id)
       get :show, params: { id: id }
     end
 
     context 'when is successful' do
       it 'return successful' do
-        do_request(user.products.first.id)
+        make_request(user.products.first.id)
         expect(response).to have_http_status(:success)
       end
 
       it 'returns the product' do
-        do_request(user.products.first.id)
+        make_request(user.products.first.id)
         expect(JSON.parse(response.body)['title']).to eql(user.products.first.title)
       end
     end
 
     context 'when is not successful' do
       it 'returns not found' do
-        do_request(0)
+        make_request(0)
         expect(response).to have_http_status(:not_found)
       end
 
       it 'returns the error message' do
-        do_request(0)
+        make_request(0)
         expect(JSON.parse(response.body)['details']).to eql("Couldn't find Product with 'id'=0")
       end
 
       it 'returns the error code 4104' do
-        do_request(0)
+        make_request(0)
         expect(JSON.parse(response.body)['error_code']).to be(4104)
       end
     end
@@ -44,49 +44,49 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
 
   describe 'GET #index' do
     context 'when is successful' do
-      def do_request
+      def make_request
         get :index
       end
 
       it 'return successful' do
-        do_request
+        make_request
         expect(response).to have_http_status(:success)
       end
 
       it 'returns the list of products' do
         title = user.products.first.title
-        do_request
+        make_request
         expect(JSON.parse(response.body).first['title']).to eql(title)
       end
     end
   end
 
   describe 'POST #create' do
-    def do_request(product)
+    def make_request(product)
       authenticate_user(user)
       post :create, params: product
     end
 
     context 'when is successful' do
       it 'returns the product' do
-        do_request({ product: { title: 'New Product', price: 10 } })
+        make_request({ product: { title: 'New Product', price: 10 } })
         expect(JSON.parse(response.body)['title']).to eql('New Product')
       end
     end
 
     context 'when is not successful' do
       it 'returns the error message' do
-        do_request({ product: { title: '', price: 10 } })
+        make_request({ product: { title: '', price: 10 } })
         expect(JSON.parse(response.body)['description']).to eql('Invalid parameter in product')
       end
 
       it 'returns bad request' do
-        do_request({ product: { title: '', price: 10 } })
+        make_request({ product: { title: '', price: 10 } })
         expect(response).to have_http_status(:bad_request)
       end
 
       it 'returns the error code 4130' do
-        do_request({ product: { title: '', price: 10 } })
+        make_request({ product: { title: '', price: 10 } })
         expect(JSON.parse(response.body)['error_code']).to be(4130)
       end
     end
@@ -94,41 +94,41 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
 
   describe 'PUT #update' do
     context 'when is successful' do
-      def do_request(id, product)
+      def make_request(id, product)
         authenticate_user(user)
         put :update, params: { id: id, product: product }
       end
 
       it 'returns the product' do
-        do_request(user.products.first.id, { title: 'New Product' })
+        make_request(user.products.first.id, { title: 'New Product' })
         expect(JSON.parse(response.body)['title']).to eql('New Product')
       end
     end
 
     context 'when parameters are invalid' do
-      def do_request
+      def make_request
         authenticate_user(user)
         put :update, params: { id: user.products.first.id, product: { title: '' } }
       end
 
       it 'returns the error message' do
-        do_request
+        make_request
         expect(JSON.parse(response.body)['description']).to eql('Invalid parameter in product')
       end
 
       it 'returns bad request' do
-        do_request
+        make_request
         expect(response).to have_http_status(:bad_request)
       end
 
       it 'returns the error code 4130' do
-        do_request
+        make_request
         expect(JSON.parse(response.body)['error_code']).to be(4130)
       end
     end
 
     context 'when user is not the owner' do
-      def do_request
+      def make_request
         other_user = create(:user, :with_products)
         other_product = other_user.products.first
         authenticate_user(user)
@@ -136,18 +136,18 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
       end
 
       it 'returns the error message' do
-        do_request
+        make_request
         message_expected = 'Product can not be deleted or updated due to being unauthorized'
         expect(JSON.parse(response.body)['description']).to eql(message_expected)
       end
 
       it 'returns unauthorized' do
-        do_request
+        make_request
         expect(response).to have_http_status(:unauthorized)
       end
 
       it 'returns the error code 4111' do
-        do_request
+        make_request
         expect(JSON.parse(response.body)['error_code']).to be(4111)
       end
     end
@@ -155,41 +155,41 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
 
   describe 'DELETE #destroy' do
     context 'when is successful' do
-      def do_request
+      def make_request
         authenticate_user(user)
         delete :destroy, params: { id: user.products.first.id }
       end
 
       it 'returns no content' do
-        do_request
+        make_request
         expect(response).to have_http_status(:no_content)
       end
     end
 
     context 'when product does not exist' do
-      def do_request
+      def make_request
         authenticate_user(user)
         delete :destroy, params: { id: 0 }
       end
 
       it 'returns not found' do
-        do_request
+        make_request
         expect(response).to have_http_status(:not_found)
       end
 
       it 'returns the error message' do
-        do_request
+        make_request
         expect(JSON.parse(response.body)['details']).to eql("Couldn't find Product with 'id'=0")
       end
 
       it 'returns the error code 4104' do
-        do_request
+        make_request
         expect(JSON.parse(response.body)['error_code']).to be(4104)
       end
     end
 
     context 'when user is not the owner' do
-      def do_request
+      def make_request
         other_user = create(:user, :with_products)
         other_product = other_user.products.first
         authenticate_user(user)
@@ -197,18 +197,18 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
       end
 
       it 'returns the error message' do
-        do_request
+        make_request
         message_expected = 'Product can not be deleted or updated due to being unauthorized'
         expect(JSON.parse(response.body)['description']).to eql(message_expected)
       end
 
       it 'returns unauthorized' do
-        do_request
+        make_request
         expect(response).to have_http_status(:unauthorized)
       end
 
       it 'returns the error code 4111' do
-        do_request
+        make_request
         expect(JSON.parse(response.body)['error_code']).to be(4111)
       end
     end
