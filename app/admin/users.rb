14f9c5ec8d4bcd4dfafe458712c8c5f5467dde49
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-12-02 16:59:47 -0300

    Fix user forms

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -7,6 +7,25 @@ ActiveAdmin.register User do
   # Uncomment all parameters which should be permitted for assignment
   #
   permit_params :email, :password, :admin
+
+  before_action :remove_password_params_if_blank, only: [:update]
+  controller do
+    def remove_password_params_if_blank
+      return if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+  end
+
+  form do |f|
+    f.inputs 'User Details' do
+        f.input :email
+        f.input :admin
+        f.input :password
+    end
+    f.actions
+  end
   #
   # or
   #
