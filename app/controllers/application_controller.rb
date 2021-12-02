4011fe19f910462228766eb024f715ad91c2bf44
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-12-02 09:18:52 -0300

    Add activeAdmin methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,29 @@ class ApplicationController < ActionController::Base
   include ErrorHandler
   include Pundit
   skip_before_action :verify_authenticity_token
+  helper_method :destroy_admin_user_session_path
+
+  def authenticate_active_admin_user!
+    if user&.admin
+      request.headers['Authorization'] = "Bearer #{JsonWebToken.encode(user_id: user.id)}"
+      current_user
+    else
+      flash[:alert] = 'Unauthorized Access!'
+      request.headers['Authorization'] = nil
+      redirect_to '/'
+    end
+  end
+
+  def destroy_admin_user_session_path
+    request.headers['Authorization'] = nil
+    '/'
+  end
+
+  def user
+    if current_user.nil?
+      authenticate_user!
+    else
+      current_user
+    end
+  end
 end
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -56,7 +56,7 @@ ActiveAdmin.setup do |config|
   #
   # This setting changes the method which Active Admin calls
   # within the application controller.
-  config.authentication_method = :authenticate_admin_user!
+  config.authentication_method = :authenticate_active_admin_user!
 
   # == User Authorization
   #
@@ -111,7 +111,7 @@ ActiveAdmin.setup do |config|
   # link. For example :get, :delete, :put, etc..
   #
   # Default:
-  # config.logout_link_method = :get
+  config.logout_link_method = :get
 
   # == Root
   #
