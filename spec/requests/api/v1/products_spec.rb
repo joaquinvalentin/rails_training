7cc2a6aafe67bd7309e75a2d9f4451f42170b8d1
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-12-03 16:09:31 -0300

    Add  update and delete docs

--- spec/requests/api/v1/products_spec.rb
+++ spec/requests/api/v1/products_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe 'api/v1/products', type: :request do
         tags 'Products'
         description 'Get the product information.'
         produces 'application/json'
-        parameter name: :id, in: :path, type: :string
+        parameter name: :id, in: :path, type: :integer
         parameter name: :Authorization, in: :header, type: :string
         response '200', 'Product found' do
           let(:id) { product.id }
@@ -54,6 +54,102 @@ RSpec.describe 'api/v1/products', type: :request do
         end
         response '500', 'Internal server error' do
           let(:id) { 'invalid' }
+          let(:Authorization) { authentication_token(user) }
+
+          document_response_without_test!
+        end
+      end
+
+      delete 'Delete a product' do
+        tags 'Products'
+        description 'Delete a product.'
+        consumes 'application/json'
+        parameter name: :Authorization, in: :header, type: :string
+        parameter name: :id, in: :path, type: :integer
+        response '204', 'Product deleted' do
+          let(:id) { product.id }
+          let(:Authorization) { authentication_token(user) }
+
+          run_test!
+        end
+        response '401', 'Unauthenticated' do
+          let(:id) { product.id }
+          let(:Authorization) { nil }
+
+          include_context 'with integration test'
+        end
+        response '403', 'Unauthorized' do
+          let(:id) { product.id }
+          let(:Authorization) { authentication_token(admin) }
+
+          include_context 'with integration test'
+        end
+        response '404', 'Product not found' do
+          let(:id) { 0 }
+          let(:Authorization) { authentication_token(user) }
+
+          include_context 'with integration test'
+        end
+        response '500', 'Internal server error' do
+          let(:id) { 'invalid' }
+          let(:Authorization) { authentication_token(user) }
+
+          document_response_without_test!
+        end
+      end
+
+      put 'Update a product' do
+        tags 'Products'
+        description 'Update a product.'
+        consumes 'application/json'
+        parameter name: :Authorization, in: :header, type: :string
+        parameter name: :id, in: :path, type: :integer
+        parameter name: :params, in: :body, schema: {
+          type: :object,
+          properties: {
+            product: {
+              type: :object,
+              properties: {
+                title: { type: :string },
+                price: { type: :integer },
+                published: { type: :boolean }
+              },
+              required: %w[title price]
+            },
+            required: %w[product]
+          }
+        }
+        response '200', 'Product updated' do
+          let(:Authorization) { authentication_token(user) }
+          let(:id) { product.id }
+          let(:params) { { product: attributes_for(:product) } }
+
+          include_context 'with integration test'
+        end
+        response '400', 'Bad parameter' do
+          let(:params) { { product: { title: 'Prod', price: -1 } } }
+          let(:id) { product.id }
+          let(:Authorization) { authentication_token(user) }
+
+          include_context 'with integration test'
+        end
+        response '401', 'Unauthenticated' do
+          let(:params) { { product: attributes_for(:product) } }
+          let(:id) { product.id }
+          let(:Authorization) { nil }
+
+          include_context 'with integration test'
+        end
+        response '403', 'Unauthorized' do
+          let(:params) { { product: attributes_for(:product) } }
+          let(:id) { product.id }
+          let(:Authorization) { authentication_token(admin) }
+
+          include_context 'with integration test'
+        end
+        response '500', 'Internal server error' do
+          let(:params) { { product: attributes_for(:product) } }
+          let(:id) { product.id }
           let(:Authorization) { authentication_token(user) }
 
           document_response_without_test!
@@ -88,9 +184,7 @@ RSpec.describe 'api/v1/products', type: :request do
           document_response_without_test!
         end
       end
-    end
 
-    path '/api/v1/products' do
       post 'Create a product' do
         tags 'Products'
         description 'Create a product.'
@@ -103,8 +197,8 @@ RSpec.describe 'api/v1/products', type: :request do
               type: :object,
               properties: {
                 title: { type: :string },
-                price: { type: :string },
-                published: { type: :string }
+                price: { type: :integer },
+                published: { type: :boolean }
               },
               required: %w[title price]
             },
@@ -114,6 +208,12 @@ RSpec.describe 'api/v1/products', type: :request do
         response '201', 'Product created' do
           let(:Authorization) { authentication_token(user) }
           let(:params) { { product: attributes_for(:product) } }
+
+          include_context 'with integration test'
+        end
+        response '400', 'Bad parameter' do
+          let(:params) { { product: { title: 'Prod', price: -1 } } }
+          let(:Authorization) { authentication_token(user) }
 
           include_context 'with integration test'
         end
