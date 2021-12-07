3016e0be55021cc7bb044053bc4ee9394e8b8076
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-12-07 14:58:10 -0300

    Fix mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 class UserMailer < ApplicationMailer
-  default from: current_user&.email
-
   def welcome_email
     @user = params[:user]
-    @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    @url = 'http://example.com/login'
+    mail(from: params[:admin], to: @user[:email], subject: 'Welcome to My Awesome Site')
   end
 end
