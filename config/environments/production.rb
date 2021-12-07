49a532809ae81de02c8d382cc3dfd3613a355a15
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-12-07 10:36:34 -0300

    Add sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,4 +113,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context =
   # ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
 end
