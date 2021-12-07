6c654a32703782258b87e357b42874be7299a903
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-12-07 15:01:37 -0300

    Letter opener configuration

---
+++ config/initializers/letter_opener.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+LetterOpener.configure do |config|
+  # Default value is `:default` that renders styled message with showing useful metadata.
+  config.message_template = :default
+  # config.message_template = :light
+end
