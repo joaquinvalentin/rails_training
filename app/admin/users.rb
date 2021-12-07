032d33b89691901e2010641a52742cc990be5528
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-12-07 15:26:46 -0300

    Sending mail in  create (CMS and API)

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -4,12 +4,17 @@ ActiveAdmin.register User do
   permit_params :email, :password, :admin
 
   before_action :remove_password_params_if_blank, only: [:update]
+  after_action :send_email, only: [:create]
   controller do
     def remove_password_params_if_blank
       return unless params[:user][:password].blank? && params[:user][:password_confirmation].blank?
 
       params[:user].delete(:password)
       params[:user].delete(:password_confirmation)
+    end
+
+    def send_email
+      UserMailer.with(user: params[:user], admin: current_user.email).welcome_email.deliver_now if user.valid?
     end
   end
 
