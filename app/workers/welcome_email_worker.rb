a515b5df8fc5c0c4204f1fa7f4da2651857cd223
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-12-08 15:32:43 -0300

    Def worker

--- app/workers/welcome_email_worker.rb
+++ app/workers/welcome_email_worker.rb
@@ -3,7 +3,7 @@
 class WelcomeEmailWorker
   include Sidekiq::Worker
 
-  def perform(*args)
-    # Do something
+  def perform(user, admin)
+    UserMailer.with(user: user, admin: admin).welcome_email.deliver_now
   end
 end
