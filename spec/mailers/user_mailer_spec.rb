0ccafd1de7cdecd881f7c33b0bbdae84fb73a497
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-12-08 17:06:52 -0300

    Fixed test

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe UserMailer, type: :mailer do
   let(:user) { create(:user) }
   let(:admin) { create(:user, :is_admin) }
-  let(:mail) { described_class.with(user: user, admin: admin.email).welcome_email }
+  let(:mail) { described_class.with(user: user.email, admin: admin.email).welcome_email }
 
   it 'correct subject' do
     expect(mail.subject).to eq('Welcome to My Awesome Site')
