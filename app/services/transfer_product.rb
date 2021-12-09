1f49350c917328a1bbe4149e7411fb3b74799b1e
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-12-09 16:38:50 -0300

    Add new policy methods and errors

--- app/services/transfer_product.rb
+++ app/services/transfer_product.rb
@@ -1,19 +1,22 @@
 # frozen_string_literal: true
 
 class TransferProduct < ServiceObject
-  def initialize(product:, new_owner:)
-    super
+  def initialize(product:, current_owner:, new_owner:)
     @product = product
     @new_owner = new_owner
+    @current_owner = current_owner
   end
 
   def call
+    return @error = 4207 unless ProductPolicy.new(current_owner, product).transfer_from?
+    return @error = 4208 unless ProductPolicy.new(new_owner, product).transfer_to?
+
     transfer_product
   end
 
   private
 
-  attr_reader :new_owner, :product
+  attr_reader :new_owner, :product, :current_owner
 
   def transfer_product
     product.user_id = new_owner.id
