748bda04c58cb1b704a3e807874d979326a81008
joaquinvalentin <joaquin.valentin@xmartlabs.com>
2021-12-14 16:48:09 -0300

    Add service specs

--- spec/services/create_user_spec.rb
+++ spec/services/create_user_spec.rb
@@ -3,26 +3,43 @@
 require 'rails_helper'
 
 RSpec.describe CreateUser, type: :service do
-  def params
-    { email: 'prueba@gmail.com', password: 'P4ssw0rd', admin: 0 }
-  end
-
   def admin
     @admin ||= create(:user, :is_admin)
   end
 
-  def service
-    @service = CreateUser
-  end
-
   def call_service(creator, user_params)
-    service.call(user_params: user_params, creator: creator)
+    CreateUser.call(user_params: user_params, creator: creator)
   end
 
   context 'when the params are valid' do
+    def params
+      { email: 'prueba@gmail.com', password: 'P4ssw0rd', admin: 0 }
+    end
+
     it 'will create the user' do
+      create_user = call_service(admin, params)
+      expect(create_user.user.email).to eql params[:email]
+    end
+  end
+
+  context 'when the params are invalid' do
+    def params
+      { email: '[].com', password: 'P4ssw0rd', admin: 0 }
+    end
+
+    it 'returns an error' do
+      create_user = call_service(admin, params)
+      expect(create_user.error?).to be true
+    end
+
+    it 'returns the error code 4104' do
+      create_user = call_service(admin, params)
+      expect(create_user.error_code).to be 4104
+    end
+
+    it 'the user is not created' do
       call_service(admin, params)
-      expect(service.user.email).to eql params.email
+      expect(User.find_by_email(params[:email])).to be nil
     end
   end
 end
